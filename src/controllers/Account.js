57569082113fb869e38240e9364d107dd4263ab2
Ren <renardchien@gmail.com>
2015-03-31 16:56:23 -0400

    added updates from class

--- src/controllers/Account.js
+++ src/controllers/Account.js
@@ -1,18 +1,22 @@
 
 var loginPage = function(req, res) {
-    
+    res.render('login');
 };
 
 var signupPage = function(req, res) {
-    
+    res.render('signup');
 };
 
 var logout = function(req, res) {
-    
+    res.redirect('/'); 
 };
 
 var login = function(req, res) {
 
+    if(!req.body.username || ! req.body.pass) {
+        return res.status(400).json({error: "RAWR! ALL FIELDS REQUIRED"});
+    }
+    
 };
 
 var signup = function(req, res) {
